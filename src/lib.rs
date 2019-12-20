2a4d9ef40d632c4b916dd1c4ee0e9470bf86a5a3
Aedius <aedius.filmania@gmail.com>
2019-12-20 21:46:32 +0100

    add first event

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+
+use serde::{Deserialize, Serialize };
+
+
+#[derive(Deserialize, Serialize)]
+pub struct AccountCreated {
+    uuid: String,
+    name: String,
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+
+        let data =  crate::AccountCreated{
+            uuid: "b7b9749f-6baf-43fa-be79-15ead7cafcca".to_string(),
+            name: "Aedius".to_string(),
+        };
+
+        assert_eq!(
+            serde_json::to_string(&data).unwrap(),
+            r#"{"uuid":"b7b9749f-6baf-43fa-be79-15ead7cafcca","name":"Aedius"}"#);
+    }
+}
