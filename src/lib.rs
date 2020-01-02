d111e8a52deec3a25284f3802140f1032b220677
Aedius <aedius.filmania@gmail.com>
2020-01-02 11:40:17 +0100

    add struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 use serde::{Deserialize, Serialize};
-use crate::AuthEvent::{Created, Login};
 
 const STREAM_NAME: &'static str = "account";
-const ACCOUNT_CREATED: &'static str = "account_created";
-const LOGGED_IN: &'static str = "logged_in";
+pub const ACCOUNT_CREATED: &'static str = "account_created";
+pub const LOGGED_IN: &'static str = "logged_in";
 
 
 #[derive(Deserialize, Serialize)]
@@ -17,27 +16,45 @@ pub struct LoggedIn {
     pub uuid: String,
 }
 
-
 #[derive(Deserialize, Serialize)]
-pub enum AuthEvent {
+pub enum AuthEventList {
     Created(AccountCreated),
     Login(LoggedIn),
     Empty
 }
 
-impl mod_event::PublicEvent for AuthEvent {
+pub struct GlobalAuthEvent{
+    events: AuthEventList,
+}
+
+impl GlobalAuthEvent {
+    pub fn from_enum(events :AuthEventList )-> Self{
+        GlobalAuthEvent{
+            events
+        }
+    }
+}
+
+impl mod_event::PublicEvent for GlobalAuthEvent {
+
     fn from_json(event_type: &str, json: &str) -> Self {
         match event_type {
             ACCOUNT_CREATED => {
                 let created: AccountCreated = serde_json::from_str(json).unwrap();
-                AuthEvent::Created(created)
+                GlobalAuthEvent {
+                    events: AuthEventList::Created(created)
+                }
             }
             LOGGED_IN => {
                 let login: LoggedIn = serde_json::from_str(json).unwrap();
-                AuthEvent::Login(login)
+                GlobalAuthEvent {
+                    events: AuthEventList::Login(login)
+                }
             }
             _ => {
-                AuthEvent::Empty
+                GlobalAuthEvent {
+                    events: AuthEventList::Empty
+                }
             }
         }
     }
@@ -47,11 +64,11 @@ impl mod_event::PublicEvent for AuthEvent {
     }
 
     fn get_json(&self) -> Result<(&'static str, String), &str> {
-        match self {
-            Created(account_created) => {
+        match &self.events {
+            AuthEventList::Created(account_created) => {
                 Ok((ACCOUNT_CREATED, serde_json::to_string(account_created).unwrap()))
             }
-            Login(logged_in) => {
+            AuthEventList::Login(logged_in) => {
                 Ok((LOGGED_IN, serde_json::to_string(logged_in).unwrap()))
             }
             _ => {
@@ -66,17 +83,44 @@ mod tests {
     use mod_event::PublicEvent;
 
     #[test]
-    fn it_works() {
-        let data = crate::AuthEvent::Created(
-            crate::AccountCreated {
-                uuid: "b7b9749f-6baf-43fa-be79-15ead7cafcca".to_string(),
-                name: "Aedius".to_string(),
-            });
+    fn lifecycle_from_enum() {
+        let logged_in = crate::LoggedIn{
+            uuid : "cbae7c51-2068-4cb4-b4c4-e118d675f277".to_string()
+        };
+        let data = crate::AuthEventList::Login(logged_in);
 
-        let (event_type, event_json) = data.get_json().unwrap();
+        let obj = crate::GlobalAuthEvent::from_enum( data);
+
+        assert_eq!(&obj.stream_name(), &"account");
+
+        let (event_type, event_json) = obj.get_json().unwrap();
+
+        assert_eq!(
+            event_json,
+            r#"{"uuid":"cbae7c51-2068-4cb4-b4c4-e118d675f277"}"#);
+
+        assert_eq!(
+            event_type,
+            "logged_in");
+    }
+
+    #[test]
+    fn lifecycle_from_json() {
+        let data = r#"{"name":"Aedius","uuid":"b7b9749f-6baf-43fa-be79-15ead7cafcca","foo":"bar"}"#;
+
+        let obj = crate::GlobalAuthEvent::from_json(crate::ACCOUNT_CREATED, data);
+
+        assert_eq!(&obj.stream_name(), &"account");
+
+        let (event_type, event_json) = obj.get_json().unwrap();
 
         assert_eq!(
             event_json,
             r#"{"uuid":"b7b9749f-6baf-43fa-be79-15ead7cafcca","name":"Aedius"}"#);
+
+        assert_eq!(
+            event_type,
+            "account_created");
     }
+
 }
